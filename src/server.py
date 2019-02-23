7248695cbbb1ce4d9eab28d5159e55eeaa46a490
tvargulev <tv1u17@soton.ac.uk>
2019-02-23 16:06:37 +0000

    updated response input

--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
 """Main file of the server implementation."""
+import subprocess
 from flask import Flask
 from flask import request
 
@@ -10,11 +11,25 @@ def hello_world():
     return 'Hello World!'
 
 
+def run_model(word,temp,poems_count):
+    module_path = "'../dl-model/cv/model.p'"
+    sample_path = "../dl-model/sample.py"
+    args = ['python2.7',sample_path ,'-m',str(module_path),'-t',str(temp),'-s',str(word),'-n',str(poems_count),'-o',"0"]
+    result = subprocess.run(args, stdout=subprocess.PIPE)
+    result = result.stdout.decode('utf-8')
+    return result
+
 @app.route('/lyrics', methods=["POST"])
-def postLyrics():
-    words = request.form["words"]
-    return words
+def post_lyrics():
+    word = request.form["word"]
+    temp = request.form["temp"]
+    poems_count =  request.form["poems_count"]
+    model_output = run_model(word,temp,poems_count)
+    return model_output
 
 
 if __name__ == '__main__':
     app.run()
+
+
+
